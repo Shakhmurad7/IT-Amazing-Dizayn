b51791b5517bd4eb1ba2e5bdc3b2cb4a8bb68e2e
Shakhmurad <muradb356@gmail.com>
2023-08-12 16:39:54 +0400

    added header

---
+++ Home-Page/add.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    autoplay: {
+      delay: 2000,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+});
