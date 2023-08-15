52dc6dc9332fbd1a1e018729a07f0256c5440816
Shakhmurad <muradb356@gmail.com>
2023-08-16 00:47:26 +0400

    add Elaqe page

--- Home-Page/script.js
+++ Home-Page/script.js
@@ -1,9 +1,11 @@
-const menu = document.querySelector('.menu-icon')
-const headerUL = document.querySelector('.header-top-center')
-const close = document.querySelector('.close-icon')
-menu.addEventListener('click' , function(){
-    headerUL.classList.toggle('active-menu')
-})
-close.addEventListener('click' , function(){
-    headerUL.classList.toggle('active-menu')
-})
+const menu = document.querySelector(".menu-icon");
+const headerUL = document.querySelector(".header-top-center");
+const close = document.querySelector(".close-icon");
+menu.addEventListener("click", function () {
+  headerUL.classList.toggle("active-menu");
+    document.body.style.overflow = 'hidden'
+});
+close.addEventListener("click", function () {
+  headerUL.classList.toggle("active-menu");
+  document.body.style.overflow = 'visible'
+});
