d546376a90f2ee2c0d4d2b807a3432f872613d0b
Shakhmurad <muradb356@gmail.com>
2023-08-18 02:51:30 +0400

    add portfoli page

--- Portfolio/index.js
+++ Portfolio/index.js
@@ -1,28 +1,47 @@
-const pElement = document.querySelectorAll('.sectin-center-text p')
 
-pElement[0].classList.add('active-p')
-    pElement.forEach((element , i)=>{
-        element.addEventListener('click' , function(){
-            for(let i = 0; i<pElement.length; i++){
-                pElement[i].classList.remove('active-p')
-            }
-            element.classList.add('active-p')
-        })
-    })
+const pElements = document.querySelectorAll('.sectin-center-text p');
 
-const sectionbigcart = document.querySelector('.section-container-cart')
-const url = `https://json-server-wigy.vercel.app/posts`
-fetch(url).then((req)=> req.json()).then((data)=>{
-    data.forEach((elem)=>{
-        console.log(elem.img[0].img)
-        sectionbigcart.innerHTML+=` 
-        <div class="section-block-cart">
-              <img src='${elem.img[0].img}'>
-              <p>lorem</p>
-               </div>
-        `
-    })
-})
+pElements[0].classList.add('active-p');
+pElements.forEach((element, i) => {
+    element.addEventListener('click', function() {
+        pElements.forEach(p => p.classList.remove('active-p'));
+        element.classList.add('active-p');
+
+        const title = encodeURIComponent(element.textContent.trim());
+        console[title]
+        const url = `https://json-server-wigy.vercel.app/posts?title=${title}`;
+        fetch(url)
+            .then(req => req.json())
+            .then(data => {
+                let cartContent = '';
+                data.forEach(elem => {
+                    cartContent += `
+                    <div class="section-block-cart">
+                          <img src="${elem.img[0].img}">
+                          <p>lorem</p>
+                    </div>`;
+                });
+                sectionbigcart.innerHTML = cartContent;
+            });
+    });
+});
+
+const sectionbigcart = document.querySelector('.section-container-cart');
+const url = `https://json-server-wigy.vercel.app/posts`;
+
+fetch(url)
+    .then(req => req.json())
+    .then(data => {
+        let cartContent = '';
+        data.forEach(elem => {
+            cartContent += `
+            <div class="section-block-cart">
+                  <img src="${elem.img[0].img}">
+                  <p>lorem</p>
+            </div>`;
+        });
+        sectionbigcart.innerHTML = cartContent;
+    });
 
 
 
